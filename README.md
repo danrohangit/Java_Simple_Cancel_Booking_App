30b7a5d001f488fec79445f49bf0b75778203a61
Muhd Dandarawie Bin Rohani <dandarohanibusiness@gmail.com>
2023-04-30 21:54:24 +0800

    cancel booking feature done

--- README.md
+++ README.md
@@ -8,10 +8,22 @@ An app to cancel booking done by customers with  java_simple_booking_webapp_2
 - I am purely attempting to learn java from scratch, and refresh myself
 
 ## Screenshots
-- Show Bookings
+- Show Bookings Button
 - 
 ![ScreenShot](https://raw.githubusercontent.com/danrohangit/Java_Simple_Cancel_Booking_App/main/images/ShowBookings1.PNG)
 
-- Cancel Bookings
+- Booking list
 - 
 ![ScreenShot](https://raw.githubusercontent.com/danrohangit/Java_Simple_Cancel_Booking_App/main/images/CancelBookings1_1.PNG)
+
+- Booking list Selected
+- 
+![ScreenShot](https://raw.githubusercontent.com/danrohangit/Java_Simple_Cancel_Booking_App/main/images/CancelBookings1_1_selected.PNG)
+
+- Booking list Cancel Prompt
+- 
+![ScreenShot](https://raw.githubusercontent.com/danrohangit/Java_Simple_Cancel_Booking_App/main/images/CancelBookings1_1_cancelledPrompt.PNG)
+
+- Booking list After Cancelled
+- 
+![ScreenShot](https://raw.githubusercontent.com/danrohangit/Java_Simple_Cancel_Booking_App/main/images/CancelBookings1_1_afterCancelled.PNG)
--- gui.java
+++ gui.java
@@ -3,19 +3,17 @@ import java.awt.Dimension;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Container;
+import java.awt.Color;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.*;
 
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumn;
+import javax.swing.table.*;
 
 class gui {
 	
@@ -61,10 +59,7 @@ class gui {
 			  
 			  if(rows.isEmpty()){
 				rows.add("No Data" + "," + "No Data" + "," + "No Data" + "," + "No Data" + "," + "No Data" + "," + "No Data");  
-			  }
-
-			  // Display the list of rows in a dialog box
-			  JOptionPane.showMessageDialog(null, rows.toString());			  
+			  }		  
 
 			  // Close the database connection
 			  rs.close();
@@ -90,64 +85,147 @@ class gui {
 		return rows;
 		
 	}
+	
 
-	static void showRows(ArrayList<String> listOfRows, JFrame frame) {
+	static void showRows(ArrayList<String> rows, JFrame frame){
 		
-		// Get the content pane of the frame
-		Container contentPane = frame.getContentPane();
-
-		// Remove any existing components from the content pane
-		contentPane.removeAll();
+		// Clear the frame's content pane
+		frame.getContentPane().removeAll();
+		frame.repaint();
 		
-		frame.setTitle("My First GUI - Cancel Bookings");
+		// Create a panel with BorderLayout and add it to the frame's content pane
+		JPanel panel = new JPanel(new BorderLayout());
+		frame.getContentPane().add(panel);
 
-		// Create a table model and add columns
-		DefaultTableModel tableModel = new DefaultTableModel();
-		String[] headers = { "ID", "Name", "Email", "Phone", "Location", "Date", "" };
-		tableModel.setColumnIdentifiers(headers);
-
-		// Add rows to the table model
-		for (String row : listOfRows) {
-			String[] rowData = row.split(",");
-			tableModel.addRow(rowData);
+		// Create a JTable with the number of rows equal to the size of the rows arraylist and six columns
+		String[] columnNames = {"ID", "Name", "Email", "Phone", "Location", "Date"};
+		Object[][] rowData = new Object[rows.size()][6];
+		for (int i = 0; i < rows.size(); i++) {
+			String[] row = rows.get(i).split(",");
+			for (int j = 0; j < row.length; j++) {
+				rowData[i][j] = row[j];
+			}
 		}
+		JTable table = new JTable(rowData, columnNames);
 
-		// Create a JTable with the table model
-		JTable table = new JTable(tableModel);
+		// Set the table's column widths
+		TableColumnModel columnModel = table.getColumnModel();
+		columnModel.getColumn(0).setPreferredWidth(30);
+		columnModel.getColumn(1).setPreferredWidth(100);
+		columnModel.getColumn(2).setPreferredWidth(150);
+		columnModel.getColumn(3).setPreferredWidth(100);
+		columnModel.getColumn(4).setPreferredWidth(100);
+		columnModel.getColumn(5).setPreferredWidth(100);
 
-		// Create a custom cell renderer for the last column
-		TableColumn column = table.getColumnModel().getColumn(6);
-		column.setCellRenderer(new ButtonRenderer());
-
-		// Set the preferred size of the table and add it to a scroll pane
-		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		// Add the table to a JScrollPane and add the JScrollPane to the panel
 		JScrollPane scrollPane = new JScrollPane(table);
+		panel.add(scrollPane, BorderLayout.CENTER);
 
-		// Add the scroll pane to the content pane's CENTER position
-		contentPane.add(scrollPane, BorderLayout.CENTER);
+		// Add a cancel button to the panel
+		JButton cancelButton = new JButton("Cancel Booking");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int selectedRow = table.getSelectedRow();
+				if (selectedRow != -1) {
+					String bookingId = (String) table.getValueAt(selectedRow, 0);
+					cancelBooking(bookingId,rows,frame);
+					rows.remove(selectedRow);
+					((AbstractTableModel) table.getModel()).fireTableDataChanged();
+				} else {
+					JOptionPane.showMessageDialog(frame, "Please select a row to cancel.");
+				}
+			}
+		});
+		
+		// Set the preferred size of the cancel button
+		cancelButton.setPreferredSize(new Dimension(150, 50));
+		
+		// Add a cancel button to a separate JPanel and add this panel to the SOUTH position of the main panel
+		JPanel buttonPanel = new JPanel(new FlowLayout());
+		buttonPanel.add(cancelButton);
+		panel.add(buttonPanel, BorderLayout.SOUTH);
 
-		// Set the content pane of the frame and make it visible
-		frame.setContentPane(contentPane);
+		// Set the frame's size and make it visible
+		frame.setPreferredSize(new Dimension(1024, 600));
+		frame.pack();
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 
-	// Custom cell renderer for the last column
-	static class ButtonRenderer extends DefaultTableCellRenderer {
-		private static final long serialVersionUID = 1L;
+	
+	public static void cancelBooking(String bookingId,ArrayList<String> rows, JFrame frame) {
+		// Perform the logic to cancel the booking with the given ID
+		// For example, you might remove the booking from a database or mark it as cancelled
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Establish a connection to the database
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/restaurantbooking", "dddd", "uhbygv123");
+			Statement stmt = conn.createStatement();
 
-		public ButtonRenderer() {
-			setOpaque(true);
-		}
+			// Execute a query to delete the row with the given ID
+			stmt.executeUpdate("DELETE FROM booking WHERE id = " + bookingId);
+			stmt.close();
+			conn.close();
+			
+			// Show a message to the user to indicate that the booking has been cancelled
+			JOptionPane.showMessageDialog(null, "Booking " + bookingId + " has been cancelled.");
 
-		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-			JButton button = new JButton("Cancel Booking");
-			button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					// Handle button click event
-					System.out.println("View button clicked for row " + row);
-				}
-			});
-			return button;
+			} catch (ClassNotFoundException ex) {
+				
+			// Show a message to the user to indicate that the booking has been cancelled
+			JOptionPane.showMessageDialog(null, "Booking " + bookingId + " CANNOT cancelled.");	
+				
+			ex.printStackTrace();
+			
+			} catch (SQLException ex) {
+				
+			// Show a message to the user to indicate that the booking has been cancelled
+			JOptionPane.showMessageDialog(null, "Booking " + bookingId + " CANNOT cancelled.");		
+				
+			ex.printStackTrace();
+		}		
+		
+		ArrayList<String> newRows = new ArrayList<String>();
+		
+		try {
+		  Class.forName("com.mysql.cj.jdbc.Driver");
+		  // Establish a connection to the database
+		  Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/restaurantbooking", "cccc", "edcrfv456");
+		  Statement stmt = conn.createStatement();
+
+		  // Execute a query to retrieve the rows from the booking table
+		  ResultSet rs = stmt.executeQuery("SELECT * FROM booking WHERE theDate >= CURDATE() ORDER BY theDate ASC");
+		  
+		  // Iterate over the result set to retrieve the rows and store them in a list or array
+		  while (rs.next()) {
+			  
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			String email = rs.getString("email");	
+			String phone = rs.getString("phone");	
+			String location = rs.getString("location");	
+			String theDate = rs.getString("theDate");	
+			
+			String row = id + "," + name + "," + email + "," + phone + "," + location + "," + theDate;
+			newRows.add(row);
+		  }
+		  
+		  if(newRows.isEmpty()){
+			newRows.add("No Data" + "," + "No Data" + "," + "No Data" + "," + "No Data" + "," + "No Data" + "," + "No Data");  
+		  }		  
+
+		  // Close the database connection
+		  rs.close();
+		  stmt.close();
+		  conn.close();
+		  
+		  showRows(newRows,frame);	
+		  
+		} catch (ClassNotFoundException ex) {
+		  ex.printStackTrace();
+		} catch (SQLException ex) {
+		  ex.printStackTrace();
 		}
 	}
 	
@@ -155,7 +233,7 @@ class gui {
 		
 		JFrame frame = new JFrame("My First GUI - Show Bookings");
 		
-		frame.setSize(1024, 768);
+		frame.setSize(1024, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Load rows
